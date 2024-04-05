2b16a418314a449148a8d44d5ddad4075600fc2a
Hailin Liu <0123liuhailin@gmail.com>
2024-04-05 16:52:48 +0300

    Created the Client class, which represents a registrated client, and corresponds a row in Client table of database,[05 APR 2024], Hailin Liu

---
+++ server/config/setting.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Establish database connection
+engine = create_engine('sqlite:///database/cybermarket.db', echo=False)
+
+# Define the base class that maps database tables and python classes
+Base = declarative_base()
+
+# Create database session
+Session = sessionmaker(bind=engine)
+
+# The custom class can be used to construct a new Session
+session = Session()
---
+++ server/model/Client.py
@@ -0,0 +1,50 @@
+from sqlalchemy import Column, Integer, String
+from server.config.setting import engine, Base, session
+
+
+class Client(Base):
+    '''
+        Client class, corresponds the table Client in
+        %database%/cybermarket.db/Client
+    '''
+    __tablename__ = 'Client'
+    clientId = Column(Integer, primary_key=True)
+    username = Column(String)
+    email = Column(String)
+    password = Column(String)
+
+    # Override the repr, when calling Client item
+    def __repr__(self):
+        return f'''
+            username: {self.username}
+            email: {self.email}
+            password: {self.password}
+        '''
+
+    def get_username(self):
+        return self.username
+
+    def set_username(self, new_username):
+        self.username = new_username
+        session.commit()
+
+    def get_email(self):
+        return self.email
+
+    def set_email(self, new_email):
+        self.email = new_email
+        session.commit()
+
+    def verify_password(self, password):
+        return self.password == password
+
+    def set_password(self, new_password, old_password):
+        if self.password == old_password:
+            self.password = new_password
+            session.commit()
+            return True
+        return False
+
+
+# Create the corresponding Tabel in database
+Base.metadata.create_all(engine)
