554557fe46b41352168d2d5f21d701445ed3e8c0
Hailin Liu <0123liuhailin@gmail.com>
2024-04-30 15:53:16 +0300

    Updated configuration files for executing unit tests, updated database files containing default merchants [30 APR 2024], Hailin Liu

--- server/setting.py
+++ server/setting.py
@@ -1,14 +1,29 @@
 """Some initialization settings for the sqlalchemy.orm framework."""
+import os
 import locale
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+
+def get_engine():
+    """Set the database engine according to the system environment."""
+    # Check the environment variables for the existence of tests
+    testing = os.getenv("CYBERMARKET")
+    if testing == "TESTING":
+        # If present, use the in-memory database
+        engine = create_engine('sqlite:///:memory:', echo=False)
+    else:
+        # If it does not exist, use the default database
+        engine = create_engine('sqlite:///database/cybermarket.db', echo=False)
+    return engine
+
+
 # Language setting
 lang = locale.setlocale(locale.LC_ALL, ("en_US", "UTF-8"))
 
 # Establish database connection
-engine = create_engine('sqlite:///database/cybermarket.db', echo=False)
+engine = get_engine()
 
 # Define the base class that maps database tables and python classes
 Base = declarative_base()
